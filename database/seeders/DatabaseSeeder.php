295f0d5624c1443f65eb350d7bb7ea7548bb8373
Satyam <satyam@Ronaks-MacBook-Pro.local>
2025-03-24 07:12:34 +0530

    Add Product Offer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ProductOffer;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(ProductSeeder::class);
         $this->call(DeliveryRuleSeeder::class);
+        $this->call(ProductOfferSeeder::class);
     }
 }
